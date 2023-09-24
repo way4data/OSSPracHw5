7d4d4cf9e720787dc61fcd34c22f23bd877f3466
way4data <gislee@dgu.ac.kr>
2023-09-24 23:32:55 +0900

    2023-09-24 OSSP1 2nd

--- screenInOut.py
+++ screenInOut.py
@@ -1,9 +1,9 @@
 # 사용자로부터부터 이름과 학번을 입력 받기
 name = input("이름을 입력하세요: ")
 student_id = input("학번을 입력하세요: ")
-dept = input("학과를 입력하세요: ")
+mydept = input("학과를 입력하세요: ")
 
 # Output inputed data
 print("이름은 ", name, "입니다.")
 print("학번은 ", student_id, "입니다.") 
-print("학과는 ", dept, "입니다.")
+print("학과는 ", mydept, "입니다.")
