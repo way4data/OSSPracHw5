1aacb18dfb4538583c4d8269e15396fb6ab5dcaa
way4data <gislee@dgu.ac.kr>
2023-09-24 22:03:32 +0900

    Subject S03

--- screenInOut.py
+++ screenInOut.py
@@ -1,6 +1,6 @@
-# 사용자로부터부터 이름과 학번 입력 받기
-name = input("이름 입력: ")
-student_id = input("학번 입력: ")
+# 사용자로부터부터 이름과 학번을 입력 받기
+name = input("이름을 입력하세요: ")
+student_id = input("학번을 입력하세요: ")
 
 # Output inputed data
 print("이름: ", name)
